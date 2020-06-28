7be2d26156465a4b92021769d228d1fb9882355e
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-06-28 19:18:37 +0200

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter.js';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter.js";
+import "./App.css";
 
 function App() {
   return (
