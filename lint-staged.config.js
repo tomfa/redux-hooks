c0c9061f8317bcd9d95e8ba491fa8ae4ba93d7dd
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-06-29 04:20:07 +0200

    Run typescript outside of lint-staged
    Running prettier that way was sync and slow.
    Running tsc via lint-staged had no benefits.
    Specifying tsconfig location is unnecessary.

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "**/*.{css,js,jsx,ts,tsx,html}": (filenames) =>
-    filenames.map((filename) => `prettier --write '${filename}'`),
-  "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
-};
