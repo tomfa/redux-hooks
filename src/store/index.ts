3b0fd9065af73afa8bef5dde7e97608c6c211285
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-08-05 22:40:40 +0200

    fiber: remove chat features

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { systemReducer } from "./system/reducers";
-import { chatReducer } from "./chat/reducers";
 import { controllerReducer } from "./controller/reducers";
 
 export const rootReducer = combineReducers({
   system: systemReducer,
-  chat: chatReducer,
   controller: controllerReducer,
 });
 
